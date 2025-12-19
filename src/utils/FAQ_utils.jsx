2fef2f2a3181526d665d7f12e13b8ea5d84d9486
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-19 15:22:14 +0545

    added

--- src/utils/FAQ_utils.jsx
+++ src/utils/FAQ_utils.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import FAQData from '../data/'
+import FAQData from '../data/FAQData'
 export default function FAQ_utils() {
   return (
     <div>
-      
+      {FAQData.length}
     </div>
   )
 }
